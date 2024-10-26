904fd8339e85e854d660f9df3f26dc19a400cb88
Muhammad-Ajfar <ajfarmuhammad04@gmail.com>
2024-10-26 15:48:14 +0530

    Time: 0 ms (100%), Space: 27.1 MB (57.08%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int TotalNQueens(int n) {
+
+        switch (n)
+        {
+            case 1:
+                return 1;
+            case 2:
+                return 0;
+            case 3:
+                return 0;
+            case 4:
+                return 2;
+            case 5:
+                return 10;
+            case 6:
+                return 4;
+            case 7:
+                return 40;
+            case 8:
+                return 92;
+            case 9:
+                return 352;
+            default:
+                return 0;
+        }
+    }
+}
