368120c2b7c2fd584fa02e0bfefd551158a5a92b
Muhammad-Ajfar <ajfarmuhammad04@gmail.com>
2025-01-03 23:03:25 +0530

    Time: 0 ms (100%), Space: 44.5 MB (27.78%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public string TriangleType(int[] nums) {
+        if(nums[0] >= nums[1] + nums[2] || nums[1] >= nums[0] + nums[2] || nums[2] >= nums[1] + nums[0]) 
+        {
+            return "none";
+        }
+        if(nums[0] == nums[1] && nums[0] == nums[2])
+        {
+            return "equilateral";
+        }
+        if(nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2])
+        {
+            return "isosceles";
+        }
+        return "scalene";
+    }
+}
