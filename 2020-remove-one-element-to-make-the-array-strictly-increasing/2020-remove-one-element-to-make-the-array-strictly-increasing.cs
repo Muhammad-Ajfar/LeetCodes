f72bebca6032cb580abd4c288353337fc08abbb1
Muhammad-Ajfar <ajfarmuhammad04@gmail.com>
2025-01-27 15:39:37 +0530

    Time: 0 ms (100%), Space: 44 MB (33.33%) - LeetHub

---
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/2020-remove-one-element-to-make-the-array-strictly-increasing.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool CanBeIncreasing(int[] nums) {
+        int count = 0;
+        
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+                
+                if (i > 1 && nums[i] <= nums[i - 2]) {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+        return true;
+    }
+}
