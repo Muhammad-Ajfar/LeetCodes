e36c2df8f0fcc21acee4ac412eb07550a77e94cb
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-05-06 23:23:33 +0530

    Time: 0 ms (100%), Space: 42.1 MB (97.3%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int FindNumbers(int[] nums) {
+        int count = 0;
+
+        foreach (int num in nums) {
+            if ((num >= 10 && num <= 99) ||
+                (num >= 1000 && num <= 9999) ||
+                num == 100000) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
