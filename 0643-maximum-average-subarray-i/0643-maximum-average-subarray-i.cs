3a3bb186065087da343d910252cb821d532257a2
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-05-18 23:30:45 +0530

    Time: 3 ms (84.09%), Space: 60.2 MB (54.5%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public double FindMaxAverage(int[] nums, int k) {
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
+        }
+        
+        int maxSum = windowSum;
+        for (int i = k; i < nums.Length; i++) {
+            windowSum += nums[i] - nums[i - k];
+            if (windowSum > maxSum) {
+                maxSum = windowSum;
+            }
+        }
+        
+        return (double)maxSum / k;
+    }
+}
