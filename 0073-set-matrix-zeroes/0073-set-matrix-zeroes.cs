ebd50ce6224989bb0464c94cc5730093605394b5
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-05-21 22:05:54 +0530

    Time: 1 ms (100%), Space: 53.4 MB (49.95%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cs
@@ -0,0 +1,66 @@
+public class Solution {
+    public void SetZeroes(int[][] matrix) {
+        int m = matrix.Length;
+        if (m == 0) return;
+        int n = matrix[0].Length;
+        
+        bool firstRowZero = false;
+        bool firstColZero = false;
+        
+        // Check if first row needs to be zeroed
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        // Check if first column needs to be zeroed
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        
+        // Use first row and column to mark zeros
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        // Zero out marked rows and columns
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Zero out first row and column if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
