507aca3174a1e527a30d3c244405868a0f08d830
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-08-08 22:09:58 +0530

    Time: 0 ms (100%), Space: 29.1 MB (42.64%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int HammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int count = 0;
+        
+        while (xor != 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+        
+        return count;
+    }
+}
