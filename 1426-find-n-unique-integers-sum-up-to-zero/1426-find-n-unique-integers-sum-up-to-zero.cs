95054638c69f21cc485279a3929441a77c2b7252
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-09-07 23:04:58 +0530

    Time: 0 ms (100%), Space: 40.2 MB (63.16%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] SumZero(int n) {
+        int[] result = new int[n];
+
+        for (int i = 0; i < n / 2; i++) {
+            result[2 * i] = i + 1;
+            result[2 * i + 1] = -(i + 1);
+        }
+        
+        return result;
+    }
+}
