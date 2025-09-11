d3b25e86237cb04813820e184db1a703b573a413
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-09-11 22:09:50 +0530

    Time: 81 ms (32.26%), Space: 58.7 MB (22.58%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cs
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cs
@@ -1,23 +1,21 @@
 public class Solution {
     public string SortVowels(string s) {
-        var pq = new PriorityQueue<char, char>();
-        var vowels = new HashSet<char>(){'a','e','i','o','u','A','E','I','O','U'};
+        var vowels = new HashSet<char>(){ 'a','e','i','o','u','A','E','I','O','U'};
+        var vowelList = new List<char>();
 
         foreach(char ch in s) {
-            if(vowels.Contains(ch))
-                pq.Enqueue(ch, ch);
+            if(vowels.Contains(ch)) vowelList.Add(ch);
         }
 
-        var sb = new StringBuilder();
+        vowelList.Sort();
 
+        var sb = new StringBuilder();
+        int idx = 0;
         foreach(char ch in s) {
-            if(vowels.Contains(ch)) 
-                sb.Append(pq.Dequeue());
-            else 
-                sb.Append(ch);
+            if(vowels.Contains(ch)) sb.Append(vowelList[idx++]);
+            else sb.Append(ch);
         }
 
         return sb.ToString();
     }
-
 }
