2f78c16167aeba731736746db8b38a1288647a90
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-10-01 23:00:00 +0530

    Time: 0 ms (100%), Space: 29 MB (68.42%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cs
+++ 1642-water-bottles/1642-water-bottles.cs
@@ -1,14 +1,5 @@
 public class Solution {
     public int NumWaterBottles(int numBottles, int numExchange) {
-        int total = numBottles;
-        int empty = numBottles;
-        
-        while (empty >= numExchange) {
-            int newBottles = empty / numExchange;
-            total += newBottles;
-            empty = empty % numExchange + newBottles;
-        }
-        
-        return total;
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 }
