27730bd05bda9ffa716c60bd54a56b633e23a526
Muhammad Ajfar <ajfarmuhammad04@gmail.com>
2025-12-04 23:30:52 +0530

    Time: 96 ms (58.9%), Space: 43.9 MB (73.63%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cs
+++ 1114-print-in-order/1114-print-in-order.cs
@@ -1,31 +1,25 @@
 using System;
 using System.Threading;
 
-public class Foo
-{
-    private AutoResetEvent secondDone = new AutoResetEvent(false);
-    private AutoResetEvent thirdDone = new AutoResetEvent(false);
+public class Foo {
+    private SemaphoreSlim second = new SemaphoreSlim(0,1);
+    private SemaphoreSlim third = new SemaphoreSlim(0,1);
 
-    public Foo()
-    {
-    }
+    public Foo() {}
 
-    public void First(Action printFirst)
-    {
+    public void First(Action printFirst) {
         printFirst();
-        secondDone.Set();   // allow second() to run
+        second.Release();
     }
 
-    public void Second(Action printSecond)
-    {
-        secondDone.WaitOne(); // wait until first() completes
+    public void Second(Action printSecond) {
+        second.Wait();
         printSecond();
-        thirdDone.Set();     // allow third() to run
+        third.Release();
     }
 
-    public void Third(Action printThird)
-    {
-        thirdDone.WaitOne(); // wait until second() completes
+    public void Third(Action printThird) {
+        third.Wait();
         printThird();
     }
 }
